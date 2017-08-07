789d224d7e70653090adfb094c2d55e73f7a8a61
Shun Takano <syunn_0702@yahoo.co.jp>
2017-08-07 14:45:07 +0000

    Add postgresql

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
     validates:title, length:{maximum: 32}, presence:true
     validates:message, length:{maximum: 256}, presence:true
     validates:name, length:{maximum: 32}
+    validates:age, length:{maximum: 2}
     
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807040812) do
+ActiveRecord::Schema.define(version: 20170807141145) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170807040812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "age"
   end
 
   create_table "users", force: :cascade do |t|
